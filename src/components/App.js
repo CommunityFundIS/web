5a78fb059dda68286deae98d1d82c82e2c87cc0a
Kristjan Mikaelsson <kristjanmik@gmail.com>
2017-11-08 04:00:39 +0000

    Fixed tests

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-
+import 'raf/polyfill';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider as ReduxProvider } from 'react-redux';
